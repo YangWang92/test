6332dedc13c92ee8711c7838a6a3fb418ed1cfec
Yang Wang <3883941+YangWang92@users.noreply.github.com>
2020-10-14 15:40:51 +0800

    Update test.js

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ function FindProxyForURL(url, host) {
 
     // use proxy for specific domains
     if (shExpMatch(host, "*.yinxiang.com"))
-        return "PROXY 127.0.0.1:7072";
+        return "PROXY 127.0.0.1:7073";
 
     // by default use no proxy
     return "DIRECT";
